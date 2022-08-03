4a73a802203410bd1595275fa4ca7688be575e20
YeoWeiHan123 <36888332+YeoWeiHan123@users.noreply.github.com>
2022-08-03 10:20:51 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,34 +59,23 @@ scrollToTop.onclick = function () {
 }
 
 //cookie pop-up
-let popUp = document.getElementById("cookiePopup");
-//When user clicks the accept button
-document.getElementById("acceptCookie").addEventListener("click", () => {
-  //Create date object
-  let d = new Date();
-  //Increment the current time by 1 minute (cookie will expire after 1 minute)
-  d.setMinutes(2 + d.getMinutes());
-  //Create Cookie withname = myCookieName, value = thisIsMyCookie and expiry time=1 minute
-  document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
-  //Hide the popup
-  popUp.classList.add("hide");
-  popUp.classList.remove("show");
-});
-//Check if cookie is already present
-const checkCookie = () => {
-  //Read the cookie and split on "="
-  let input = document.cookie.split("=");
-  //Check for our cookie
-  if (input[0] == "myCookieName") {
-    //Hide the popup
-    popUp.classList.add("hide");
-    popUp.classList.remove("show");
-  } else {
-    //Show the popup
-    popUp.classList.add("show");
-    popUp.classList.remove("hide");
+$(document).on('ready', function() {
+  if (document.cookie.indexOf("accepted_cookies=") < 0) {
+    $('.cookie-overlay').removeClass('d-none').addClass('d-block');
   }
-};
+
+  $('.accept-cookies').on('click', function() {
+    document.cookie = "accepted_cookies=yes;"
+    $('.cookie-overlay').removeClass('d-block').addClass('d-none');
+  })
+
+  // expand depending on your needs
+  $('.close-cookies').on('click', function() {
+    $('.cookie-overlay').removeClass('d-block').addClass('d-none');
+  })
+})
+
+
 /* //Check if cookie exists when page loads
 window.onload = () => {
   setTimeout(() => {
